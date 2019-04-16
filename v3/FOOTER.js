831ab280cf73b3ecbcab63b06df92fdf58788ffb
mjj0324 <mjj0324@gmail.com>
2019-04-16 17:56:49 +0900

    V3생성-1.JSON파일로 데이터 분리 및 파싱

---
+++ v3/FOOTER.js
@@ -0,0 +1,21 @@
+var FOOTER;
+(function(){
+    var footer;
+
+    FOOTER = {
+        init:function(){
+            footer = Alucard.Dom('footer').S(
+                '@id', 'footer'
+                //'<', wrap
+            );
+
+            return footer;
+        },
+        destroy:function(){
+
+        },
+        replaceContents:function(){
+
+        }
+    };
+})();
